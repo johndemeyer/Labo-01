184d686ebc896e037a2b220a00001af3331cbad4
drax_ <drax_@DESKTOP-MNHMACE>
2023-09-27 21:51:11 +0200

    Add project files.

---
+++ Labo-01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Labo_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            string d = Convert.ToString(c);
+            // oke - Console.WriteLine(c);
+            // deze is korter, in de console classe zit functie om te converteren
+            Console.WriteLine(a + b);
+
+            // labo-02
+            double aa = 2.3;
+            double bb = 4.4;
+            double cc = aa + bb;    
+            string dd = Convert.ToString(cc);
+            Console.WriteLine(dd);
+
+            // labo-03
+            Console.WriteLine("type een tekst");
+            string aaa = Console.ReadLine();
+            Console.WriteLine(aaa);
+             
+
+            // labo-04
+            Console.WriteLine("geef eerste getal");
+            double getala = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("geef een tweede getal");
+            double getalb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(getala + getalb);
+
+
+
+        }
+    }
+}
---
+++ Labo-02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Labo_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deel-01
+            float PiF = 3.14f;
+            Console.WriteLine(PiF);
+
+            //// deel-02
+            decimal dec = 3.68m;
+            Console.WriteLine(dec);
+
+            //// deel-03
+            int maxInt = int.MaxValue;
+            Console.WriteLine(maxInt);
+
+            //// deel-04
+            Console.WriteLine(++maxInt);
+
+            //// deel-05
+            ushort maxUShort = ushort.MaxValue;
+            Console.WriteLine(++maxUShort);
+
+        }
+    }
+}
