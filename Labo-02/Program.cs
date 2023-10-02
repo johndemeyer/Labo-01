189974a628739cd4378535ec22056b9ef7d96ec8
drax_ <drax_@DESKTOP-MNHMACE>
2023-10-02 21:07:31 +0200

    first commit

--- Labo-02/Program.cs
+++ Labo-02/Program.cs
@@ -5,23 +5,63 @@
         static void Main(string[] args)
         {
             // deel-01
-            float PiF = 3.14f;
+            float PiF = 3.14F;
+            Console.WriteLine("De waarde van PI");
             Console.WriteLine(PiF);
 
-            //// deel-02
-            decimal dec = 3.68m;
+            // deel-02
+            Console.WriteLine("");
+            decimal dec = 2000.25M;
+            Console.WriteLine("Dit is een decimaal");
             Console.WriteLine(dec);
 
-            //// deel-03
+            // deel-03
             int maxInt = int.MaxValue;
+            Console.WriteLine("");
+            Console.WriteLine("de waarde van maxInt");
             Console.WriteLine(maxInt);
 
-            //// deel-04
+            // deel-04
+            // dit geeft verkeerd resultaat,
+            // omdat we de waarde van maxInt met 1 verhogen,
+            // we duwen de bits door en krijgen nu het kleinste int
+            // om een juist resultaat te bekomen moeten we nog een variable declaren
+            // van het datatype long
+            Console.WriteLine("");
+            Console.WriteLine("de verkeerde waarde van maxInt +1, we verhogen de variable");
             Console.WriteLine(++maxInt);
 
-            //// deel-05
+            Console.WriteLine("");
+            Console.WriteLine(" de waarde van de maxInt");
+            int newMaxInt = int.MaxValue;
+            Console.WriteLine(newMaxInt);
+            // dit verhoogd de variable weer
+            // long newLong = (++newMaxInt);
+            Console.WriteLine("");
+            Console.WriteLine("de waarde van de long");
+            // 1 van de waarden moet een long zijn, anders verwerking als int
+            long newLong = 1L + newMaxInt;
+            Console.WriteLine(newLong);
+
+            // deel-05
             ushort maxUShort = ushort.MaxValue;
+            Console.WriteLine("");
+            Console.WriteLine("de waarde van mxUshort");
+            Console.WriteLine(maxUShort);
+            // dit kan, het is MaxUshort + 1, console.writeline maakt er een int van
+            // de value van maxUshort is niet aangepast
+            Console.WriteLine("");
+            Console.WriteLine("de juiste waarde van een mxUShort +1, variable is niet verhoogd");
+            Console.WriteLine(maxUShort + 1);
+            // dit klopt niet, want het is een overload van de short value
+            // we ztten hier de waarde van maxUshort met 1 hoger, wat niet kan
+            // op de achter grond worden alle bits door geduwd en we krijgen nu nul
+            //
+            Console.WriteLine("");
+            Console.WriteLine("een verkeerde weergave van maxUShort, variable is verhoogd");
             Console.WriteLine(++maxUShort);
+            
+
 
         }
     }
---
+++ overzicht-01/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace overzicht_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Datatypes
+
+              De primitieve datatypes
+
+              gehele getallen (integers)
+                sbyt, byte, short, ushort, int , uint, long, ulong
+                 als het eerste charatacter een 's is is dit signed
+                  (laatste bit is gebruikt voor het aanduiden of dit een neg of pos getal is)
+                  als het eerste charatacter een 'u is is dit unsigned
+                  (waar dit dta type normaal signed is, komt het nu unsigned)
+
+               komma getallen (real)
+                double, float, decimal
+
+              tekst
+              char, string
+
+             booleans
+              bool
+
+             Ieder datatype wordt gedefinieerd door minstens volgende eigenschappen:
+
+
+            
+            */
+            // test literal
+          
+
+
+            
+            
+        }
+    }
+}
